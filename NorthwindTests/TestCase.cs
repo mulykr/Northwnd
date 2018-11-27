b8199ac059bcc58dd91c452dc8a24435fbd33f55
Tanianatania <tatiana.gutiy@gmail.com>
2018-11-27 12:59:44 +0200

    QA Test on Q2

--- NorthwindTests/TestCase.cs
+++ NorthwindTests/TestCase.cs
@@ -26,6 +26,21 @@ namespace NorthwindTests
             Assert.AreEqual("Callahan", res[0]);
         }
 
+        [TestMethod]
+        public void Q2()
+        {
+            var q = _queries.Q2();
+            var res = ExecuteQuery(q, "LastName", "FirstName");
+            Assert.AreEqual(4, res.Count);
+            var result = new Dictionary<object, object>();
+            result.Add("Buchanan", "Steve");
+            result.Add("Suyama", "Michael");
+            result.Add("King", "Robert");
+            result.Add("DodsWorth", "Anne");
+            Assert.AreEqual(res,result);
+            //NORTHWND_QA
+        }
+
         public List<object> ExecuteQuery(string query, string ordinal)
         {
             var command = new SqlCommand(query, _connection);
@@ -34,6 +49,19 @@ namespace NorthwindTests
             while (reader.Read())
             {
                 objs.Add(reader.GetValue(reader.GetOrdinal(ordinal)));
+            }
+
+            return objs;
+        }
+
+        public Dictionary<object,object> ExecuteQuery(string query, string ordinal, string ordinal2)
+        {
+            var command = new SqlCommand(query, _connection);
+            var reader = command.ExecuteReader();
+            var objs = new Dictionary<object,object>();
+            while (reader.Read())
+            {
+                objs.Add(reader.GetValue(reader.GetOrdinal(ordinal)),reader.GetOrdinal(ordinal2));
             }
 
             return objs;
